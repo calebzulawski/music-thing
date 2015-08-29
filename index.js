74fe1993c7eb8c50de47ba6c062aa228361c707c
nickells <ellsworth.nick@gmail.com>
2015-08-29 13:55:00 -0400

    sends file

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello world!');
-});
+
 
 app.use('/', express.static('public'));
 
-var server = app.listen(8080, function() {
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
 
