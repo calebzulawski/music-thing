2e594fabeb68d4ed9b4b9dad9636db78d4316cae
Caleb Zulawski <caleb.zulawski@gmail.com>
2015-08-29 13:44:51 -0400

    Use express

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE \"html\">");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World!");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello world!');
 });
 
-server.listen(8080);
-console.log("Server is listening");
+app.use('/', express.static('public'));
+
+var server = app.listen(8080, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Listening at http://%s:%s', host, port);
+});
