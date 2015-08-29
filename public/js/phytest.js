8d073e5daf25c31ea6363a5872d315c98944c3df
nickells <ellsworth.nick@gmail.com>
2015-08-29 17:07:59 -0400

    id identified

--- public/js/phytest.js
+++ public/js/phytest.js
@@ -6,6 +6,13 @@ var Engine = Matter.Engine,
 // create a Matter.js engine
 var engine = Engine.create($("#phy-engine")[0]);
 
+//events
+var Events = Matter.Events
+
+//mouse constraint
+var mouseConstraint = Matter.MouseConstraint.create(engine)
+World.add(engine.world, mouseConstraint)
+
 var phyEngine = function () {
 	// create two boxes and a ground
 	var boxA = Bodies.rectangle(400, 200, 80, 80);
@@ -17,11 +24,15 @@ var phyEngine = function () {
 	// add all of the bodies to the world
 	World.add(engine.world, [boxA, boxB]);
 
+
+	//add mouse constraint
+
+
 	// run the engine
 	Engine.run(engine);
 
 	console.log('Physics engine loaded.');
-	var Events = Matter.Events
+
 
 	//detect collisions
 	Events.on(engine, "collisionStart", function (event) {
@@ -29,19 +40,50 @@ var phyEngine = function () {
 		// var randPitch = pitches[Math.round(Math.random() * 3)]
 		for (var i = 0; i < event.pairs.length; i++) {
 			// tones.play(randPitch)
-			console.log(event.pairs[i].bodyA)
-			console.log(event.pairs[i].bodyB)
+			// console.log(event.pairs[i].bodyA)
+			// console.log(event.pairs[i].bodyB)
 		}
 	})
 }
-
 var newBox = function () {
 	var thisBox = Bodies.rectangle(400, 200, 80, 80, {
-		isStatic: true
+		isStatic: true,
+		inertia: Infinity,
+		frictionAir: 1
 	});
 	World.add(engine.world, thisBox)
 }
 
 
-// $('#newBox').on('click', newBox)
+
+//detect clicks
+Events.on(engine, 'tick', function (event) {
+	//if clicking anything
+	if (mouseConstraint.mouse.button == 0) {
+		//if clicking a body
+		if (mouseConstraint.constraint.bodyB.isStatic) {
+			//console.log the body
+			console.log(mouseConstraint.constraint.bodyB.id)
+
+			//look up number five, and change its position! thanks,nick.
+			mouseConstraint.constraint.bodyB.position.x = mouseConstraint.mouse.position.x
+				// console.log(mouseConstraint.constraint.bodyB)
+		}
+		// if (mouseConstraint.mouse) {
+		// 	console.log(mouseConstraint.mouse.)
+		// }
+	}
+
+	// 	var options2 = {
+	// 		isStatic: false,
+	// 		angle: Math.PI * 0.35,
+	// 		friction: 0.0001
+	// 	}
+	// 	var elastic2 = Bodies.rectangle(300, 245, 75, 75, options2);
+	// 	World.add(engine.world, elastic2);
+	// }
+
+});
+
+$('#newBox').on('click', newBox)
 $(document).ready(phyEngine);
